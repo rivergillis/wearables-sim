020f87e2cf16e375f9e59556d95d4f0b24bad2d1
River Gillis <jrivergillis@gmail.com>
2019-04-04 17:30:38 -0500

    Generate deltas, not absolutes

--- sim.py
+++ sim.py
@@ -20,15 +20,21 @@ device_response = r.json()['devices']
 devices = [dev['_id'] for dev in device_response]
 print(devices)
 
+# Keep track of whatever we last posted so we can base future data off it
+last_payload = {devid:{'temp':0, 'humidity': 0} for devid in devices}
+
 # Post until user quits the program
 while True:
     for device in devices:
         # generate some random numbers for each device and post it
-        temp = random.uniform(0, 100)
-        humidity = random.uniform(0, 100)
-        payload = {'temp': {'value': temp, 'units': 'F'}, 'humidity': {'value': humidity, 'unit': 'g/m3'}}
-        print(f'Setting temp {temp}, humidity {humidity} for device {device}')
+        temp = random.uniform(-2, 2) + last_payload[device]['temp']
+        last_payload[device]['temp'] = temp
+        humidity = random.uniform(-2, 2) + last_payload[device]['humidity']
+        last_payload[device]['humidity'] = humidity
+
+        payload = {'temp': {'value': temp, 'units': 'F'}, 'humidity': {'value': humidity, 'units': 'g/m3'}}
+        print(f'Setting temp: {temp}, humidity: {humidity} for device {device}')
         r = requests.post(
             BASE_URL + '/devices/' + device, json={'payload': payload}, headers={'Authorization': 'Bearer ' + token})
         print(r.json()['message'])
-    time.sleep(1)
+    time.sleep(random.uniform(2, 5))
